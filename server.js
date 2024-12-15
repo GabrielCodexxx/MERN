b563de6518ddb27a1bb761e79a187f0390bb4190
Gabriel Portillo <shotishoti092403@gmail.com>
2024-12-15 23:10:00 +0800

    CHAPTER 3 done

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
-const express = require('express');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
 const path = require('path')
 const { logger } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const corsOptions = require('./config/corsOptions')
-const PORT = process.env.PORT || 35001;
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
+const { logEvents } = require('./middleware/logger')
+const PORT = process.env.PORT || 35002;
 
+console.log(process.env.NODE_ENV)
+
+connectDB()
 app.use(logger)
-
 app.use(cors(corsOptions))
-
 app.use(cors())
-
 app.use(express.json())
-
 app.use(cookieParser())
-
-app.use('/',express.static(path.join(__dirname, 'public')))
-
+app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
 
-app.all('*',(req ,res) => {
+app.all('*', (req, res) => {
     res.status(404)
-    if (req.accepts('html')){
-        res.sendFile(path.join(__dirname,'views','404.html'))
-    } else if (req.accepts('json')){
-        res.json({message: '404 Not Found'})
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
     } else {
         res.type('txt').send('404 Not Found')
     }
@@ -35,6 +36,14 @@ app.all('*',(req ,res) => {
 
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); 
+})
 
-// Chapter 3 tom
+mongoose.connection.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+    logEvents(`${err.name}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log'); 
+});
+
+//chapter 4 tom
